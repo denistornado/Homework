bc14bba30c80f0d8dde43bf3c2a780e7ca0bb0b4
denistornado <skorokhod1997@gmail.com>
2018-11-09 15:10:42 +0300

    Js Ts colors v0.0.1

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,16 @@
 // Your code here...
+
+var image = document.getElementById("myImage");
+function y (){
+   if (image.getAttribute("src") != "tshirt_yellow.jpg"){
+      image.src = "tshirt_yellow.jpg"}
+
+}
+ function w (){
+   if (image.getAttribute("src") != "tshirt_white.jpg"){
+      image.src = "tshirt_white.jpg"}
+}
+ function g (){
+   if (image.getAttribute("src") != "tshirt_green.jpg"){
+      image.src = "tshirt_green.jpg"}
+}
