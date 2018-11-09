e8d5d024a0bb0166c55d152c6501ccbd45e79acb
denistornado <skorokhod1997@gmail.com>
2018-11-10 02:37:21 +0300

    Js shirt color worked

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,16 +1,17 @@
-// Your code here...
+
 
 var image = document.getElementById("myImage");
 function y (){
-   if (image.getAttribute("src") != "\img\tshirt_yellow.jpg){
-      image.src = "\img\tshirt_yellow.jpg"}
+   if (image.getAttribute("src") != "img/tshirt_yellow.jpg"){
+      image.src = "img/tshirt_yellow.jpg"
+}
 
 }
  function w (){
-   if (image.getAttribute("src") != "\img\tshirt_white.jpg"){
-      image.src = "\img\tshirt_white.jpg"}
+   if (image.getAttribute("src") != "img/tshirt_white.jpg"){
+      image.src = "img/tshirt_white.jpg"}
 }
  function g (){
-   if (image.getAttribute("src") != "..\img\tshirt_green.jpg"){
-      image.src = "\img\tshirt_green.jpg"}
+   if (image.getAttribute("src") != "img/tshirt_green.jpg"){
+      image.src = "img/tshirt_green.jpg"}
 }
