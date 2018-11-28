ce04bc15ff313e710f384cbbae904e64719e3f29
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-28 22:42:29 +0000

    Deploy denistornado/Homework to github.com/denistornado/Homework.git:gh-pages

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+var image = document.getElementById("myImage");
+function y (){
+   if (image.getAttribute("src") != "img/tshirt_yellow.jpg"){
+      image.src = "img/tshirt_yellow.jpg"
+}
+
+}
+ function w (){
+   if (image.getAttribute("src") != "img/tshirt_white.jpg"){
+      image.src = "img/tshirt_white.jpg"}
+}
+ function g (){
+   if (image.getAttribute("src") != "img/tshirt_green.jpg"){
+      image.src = "img/tshirt_green.jpg"}
+}
